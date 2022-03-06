d79be90ca4dae670490c7160632677b8d993f510
Riley Kroes <74650481+RileyKroes11@users.noreply.github.com>
2022-03-06 13:57:22 +0000

    4 tabellen total 1:n en n:m

--- db/create.sql
+++ db/create.sql
@@ -38,17 +38,40 @@ insert into products (name, description, code, price, state, maintenance ) value
    
 insert into products (name, description, code, price, state, maintenance ) values ('Donkey Kong', 'Donkey Kong is an arcade game developed by Nintendo, released in 1981. It is an early example of the platform genre, as the gameplay focuses on maneuvering the main character across a series of platforms while dodging obstacles. ' , '492662523-7', 175, 'Brand-new', 1);
 
-
+   
 CREATE TABLE maintenance (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   explanation TEXT,
   temperature NUMERIC(10, 2),
-  design VARCHAR(15)
+  design VARCHAR(15),
+  extra_parts NUMERIC(10, 2),
+  customization NUMERIC(10, 2)
 );
   
 
-insert into maintenance (design, explanation, temperature ) values ('Royalty', 'The royalty design is these days made specifically for collecters which means it requires a lot of maintenance to keep it looking pretty, the temperature is very important here because you need to make sure nothing gets too cold or to warm for the machine.', 15-20);
+insert into maintenance (design, explanation, temperature ) values ('Royalty', 'The royalty design is these days made specifically for collecters which means it requires a lot of maintenance to keep it looking pretty, the temperature is very important here because you need to make sure nothing gets too cold or to warm for the machine to prevent it from breaking.', 15-20);
 insert into maintenance (design, explanation, temperature ) values ('Upright', 'The upright design is mostly bought by people who want to make their own personal arcade in their home, surrounded by mostly other machines it is advised to keep the temperature low as to not overheat to close-standing machines ', 11-17);
-insert into maintenance (design, explanation, temperature ) values ('Bar-top', 'The bartop design is as the name suggest on bar-tops, with its smaller design it is built to be a lot more durable. It is advised to make sure you avoid taking the bar-top to extreme temperatures', 0-30);
+insert into maintenance (design, explanation, temperature ) values ('Bar-top', 'The bartop design is as the name suggests on bar-tops, with its smaller design it is built to be a lot more durable. It is advised to make sure you avoid taking the bar-top to extreme temperatures', 0-30);
 
   
+CREATE TABLE extra_parts (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ buttons TEXT,
+ joysticks TEXT,
+ price NUMERIC(10, 2)
+);
+
+insert into extra_parts (buttons, joysticks, price) values ('0 buttons', '0 joysticks', 0); 
+insert into extra_parts (buttons, joysticks, price) values ('2 buttons', '1 joysticks', 10);
+insert into extra_parts (buttons, joysticks, price) values ('4 buttons', '2 joysticks', 17);
+
+CREATE TABLE customization (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ parts TEXT,
+ price NUMERIC(10, 2)
+);
+
+insert into customization (parts, price) values ('The original: The original design of the arcade machine.', 50); 
+insert into customization (parts, price) values ('The enhanced: A more vibrant and wild interpretation loosely based of of the original.', 100); 
+insert into customization (parts, price) values ('The throwback: A design based on what arcade machines looked like around the 1980s.', 70); 
+
